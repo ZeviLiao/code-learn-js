e609c286b6d9506b993c0867c57f937b379610f3
Zevi Liao <jointjm@gmail.com>
2024-12-29 08:25:58 +0800

    Initial commit

---
+++ src/100-tips-and-tricks/5-1.jsx
@@ -0,0 +1,30 @@
+import useUser from "./5-2";
+
+const MainPage = () => {
+  return (
+    <main>
+      <NavBar />
+      <div>Dashboard</div>
+    </main>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <aside>
+      <Heading />
+    </aside>
+  );
+};
+
+const Heading = () => {
+  return <DisplayUser />;
+};
+
+const DisplayUser = () => {
+  const { user } = useUser();
+
+  return <></>;
+};
+
+export default MainPage;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Parent from "./100-tips-and-tricks/7";
+
+export default function App() {
+  return (
+    <>
+      <Parent />
+    </>
+  );
+}
